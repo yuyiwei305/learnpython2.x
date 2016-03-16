96ef795d3069d34ecab867f016332e5dfede6ece
root <root@VM_12_205_centos.(none)>
2016-03-16 16:42:25 +0800

    xiugai myabs

--- my_abs.py
+++ my_abs.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python
 
 def my_abs(x):
+	
+	if not isinstance(x, ( int, float) ):
+
+		raise TypeError("bad operand type!")
 
 	if x>=0:
 		return x
 	else:
 		return -x
 
-
-a = raw_input("pls echo a int code : ") 
-a = int(a)
+a=10
 print my_abs(a)
+print " a is a intager"
+b=-20
+print my_abs(b)
+print "b is a fu shu "
+c= "a"
+print my_abs(c)
+print " c is a zifu"
